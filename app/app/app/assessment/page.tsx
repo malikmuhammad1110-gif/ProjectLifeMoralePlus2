8fea8b486d41c8223deb9a4b1c4c130f2715da26
malikmuhammad1110-gif <malik.muhammad1110@gmail.com>
2025-10-16 23:45:50 -0400

    Create page.tsx

---
+++ app/app/app/assessment/page.tsx
@@ -0,0 +1,9 @@
+// app/assessment/page.tsx
+export default function AssessmentPage() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Life Morale Assessment</h1>
+      <p>The interactive survey UI will go here next.</p>
+    </main>
+  );
+}
